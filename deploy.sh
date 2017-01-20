358839414431e508a6c4417aa28f376e9490fdcb
Tsvetomir Nikolov <tsvetomir.nikolov@mentormate.com>
2017-01-20 14:04:00 +0200

    Fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-function doPush {
-  echo "Pushing to GitHub."
-  cd dist
-  git init
-  git config user.name "Travis-CI"
-  git config user.email "travis@nodemeatspace.com"
-  git add .
-  git commit -m "Deployed"
-  git push --force --quiet "https://${GITHUB_API_KEY}@${REPO_REF}" master > /dev/null 2>&1
-}
 
 echo $TRAVIS_BRANCH;
 if [[ $TRAVIS_BRANCH == $SOURCE_BRANCH ]]
@@ -20,7 +8,13 @@ if [[ $TRAVIS_BRANCH == $SOURCE_BRANCH ]]
   echo "Build starting."
   ng build --prod --aot
 
-  doPush
-  exit 0
+  echo "Pushing to GitHub."
+  cd dist
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@nodemeatspace.com"
+  git add .
+  git commit -m "Deployed"
+  git push --force --quiet "https://${GITHUB_API_KEY}@${REPO_REF}" master > /dev/null 2>&1
 fi
 
