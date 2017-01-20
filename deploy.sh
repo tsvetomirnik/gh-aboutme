7afa91bc93d22537fe3c018b6ed1459d584c093e
Tsvetomir Nikolov <tsvetomir.nikolov@mentormate.com>
2017-01-20 13:49:26 +0200

    Improved deploy.sh to start for specific branch

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-git checkout develop
-rm -rf dist || exit 0
-mkdir dist
-ng build --prod --aot
-(
+set -e # Exit with nonzero exit code if anything fails
+
+function doPush {
+  echo "Pushing to GitHub."
   cd dist
   git init
   git config user.name "Travis-CI"
@@ -11,4 +10,16 @@ ng build --prod --aot
   git add .
   git commit -m "Deployed"
   git push --force --quiet "https://${GITHUB_API_KEY}@${REPO_REF}" master > /dev/null 2>&1
-)
+}
+
+if [[ $TRAVIS_BRANCH == $SOURCE_BRANCH ]]
+  git checkout develop
+  rm -rf dist || exit 0
+  mkdir dist
+  echo "Build starting."
+  ng build --prod --aot
+
+  doPush
+  exit 0
+fi
+
